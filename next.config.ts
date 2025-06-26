fdca75edb1f702d7fb1d1c52ef7ddcafaa8a4cf4
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2025-06-26 11:13:30 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,29 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.harbaacv.com',
+          },
+        ],
+        destination: 'https://harbaacv.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
+
+export default nextConfig;
+
+
 
 export default nextConfig;
